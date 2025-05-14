6078db22bd8ba3a8123fca03a5d0faea3fbeba2d
Mathieu Salz <mathieusalz1@gmail.com>
2025-05-14 14:51:36 +0200

    moved more stuff into data_utils

--- training.py
+++ training.py
@@ -4,16 +4,14 @@ import numpy as np
 from datasets import Dataset
 
 import torch
-from torch.utils.data import DataLoader
 from torch.optim import AdamW
 
-from transformers import BertTokenizer, BertForSequenceClassification, get_scheduler, DataCollatorWithPadding
+from transformers import BertTokenizer, BertForSequenceClassification, get_scheduler   
 from sklearn.preprocessing import LabelEncoder
-from sklearn.utils.class_weight import compute_class_weight
 from tqdm import tqdm
 
 from training_utils import evaluate
-from data_utils import get_data, language_weights
+from data_utils import get_data, language_weights, get_dataloaders, get_class_weights
 
 
 def training(eval_type, pretrain, batch_size, learning_rate, num_epochs, weight_decay, freeze = False, debug = False):
@@ -22,12 +20,7 @@ def training(eval_type, pretrain, batch_size, learning_rate, num_epochs, weight_
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # class_weights = compute_class_weight(class_weight='balanced',
-    #                                     classes=np.unique(train_df['language']),
-    #                                     y=train_df['language'])
-    # class_weights = torch.tensor(class_weights, dtype=torch.float).to(device)
-
-
+    class_weights = get_class_weights(train_dataset, device)
     lang_weights = language_weights(train_dataset)
 
     # Tokenizer and label encoding
@@ -36,40 +29,7 @@ def training(eval_type, pretrain, batch_size, learning_rate, num_epochs, weight_
     label_encoder = LabelEncoder()
     label_encoder.fit(train_dataset["target"])
 
-    def preprocess(example):
-        encoding = tokenizer(example["tweet"], truncation=True, padding=False, max_length=128)
-        encoding["label"] = label_encoder.transform([example["target"]])[0]
-        encoding["language"] = example["language"]
-        return encoding
-
-    train_dataset = train_dataset.map(preprocess)
-    val_dataset = val_dataset.map(preprocess)
-    test_dataset = test_dataset.map(preprocess)
-
-    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-
-    def data_collator_with_language(batch):
-        languages = [example["language"] for example in batch]
-        batch_without_language = [{k: v for k, v in example.items() if k != "language"} for example in batch]
-        batch_padded = data_collator(batch_without_language)
-        batch_padded["language"] = languages
-        return batch_padded
-
-    columns_to_remove = ["tweet", "target"]
-    if "__index_level_0__" in train_dataset.column_names:
-        columns_to_remove.append("__index_level_0__")
-
-    train_dataloader = DataLoader(
-        train_dataset.remove_columns(columns_to_remove),
-        batch_size=batch_size, shuffle=True, collate_fn=data_collator_with_language)
-
-    val_dataloader = DataLoader(
-        val_dataset.remove_columns(columns_to_remove),
-        batch_size=batch_size, collate_fn=data_collator_with_language)
-
-    test_dataloader = DataLoader(
-        test_dataset.remove_columns(columns_to_remove),
-        batch_size=batch_size, collate_fn=data_collator_with_language)
+    train_dataloader, val_dataloader, test_dataloader = get_dataloaders(tokenizer, label_encoder, batch_size, train_dataset, val_dataset, test_dataset)
 
     num_labels = len(label_encoder.classes_)
     model = BertForSequenceClassification.from_pretrained(pretrain, num_labels=num_labels)
@@ -77,14 +37,11 @@ def training(eval_type, pretrain, batch_size, learning_rate, num_epochs, weight_
     model.to(device)
 
     if freeze:
-        # Freeze all layers except the classifier
         for param in model.bert.parameters():
             param.requires_grad = False
 
-        # Keep only the classification head trainable
         for param in model.classifier.parameters():
             param.requires_grad = True
-
 
     optimizer = AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
