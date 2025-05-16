ea3824d177fdbdd95f8e65f9acef796341453f22
coester <timothee.coester@epfl.ch>
2025-05-16 16:31:18 +0200

    cleaned training and unbalance sampler

---
+++ data_utils2.py
@@ -0,0 +1,105 @@
+import pandas as pd
+from datasets import Dataset
+from torch.utils.data import DataLoader, WeightedRandomSampler
+from transformers import DataCollatorWithPadding
+import torch
+from sklearn.utils.class_weight import compute_class_weight
+import numpy as np
+
+def get_data(debug = False):
+
+    train_data = pd.read_csv("processed_data/train.csv")
+    test_data = pd.read_csv("processed_data/test.csv")
+
+    if debug:
+
+        train_data = train_data.sample(frac = 0.01, random_state = 1)
+        test_data = test_data.sample(frac = 0.01, random_state=1)
+
+    train_dataset = Dataset.from_pandas(train_data)
+    test_dataset = Dataset.from_pandas(test_data)
+
+    print(f"Size Training Set: {len(train_dataset)} \t Size Test Set: {len(test_dataset)}")
+
+    return train_dataset, test_dataset
+
+def get_language_weights(data):
+
+    lang_counts = pd.Series(data['language']).value_counts()
+    lang_weights = 1.0 / lang_counts
+    lang_weights = (lang_weights / lang_weights.sum()).to_dict()  # normalize
+
+    print(lang_weights)
+
+    return lang_weights
+
+def get_dataloaders(tokenizer, label_encoder, batch_size, train_dataset, test_dataset,
+                    class_imbal=False, lang_imbal=False):
+
+    def preprocess(example):
+        encoding = tokenizer(example["tweet"], truncation=True, padding=False, max_length=128)
+        encoding["label"] = label_encoder.transform([example["target"]])[0]
+        encoding["language"] = example["language"]
+        return encoding
+
+    train_dataset = train_dataset.map(preprocess)
+    test_dataset = test_dataset.map(preprocess)
+
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+    def data_collator_with_language(batch):
+        languages = [example["language"] for example in batch]
+        batch_without_language = [{k: v for k, v in example.items() if k != "language"} for example in batch]
+        batch_padded = data_collator(batch_without_language)
+        batch_padded["language"] = languages
+        return batch_padded
+
+    columns_to_remove = ["tweet", "target"]
+    if "__index_level_0__" in train_dataset.column_names:
+        columns_to_remove.append("__index_level_0__")
+
+    # Prepare sampler if needed
+    if class_imbal or lang_imbal:
+        weights = get_sample_weights(train_dataset, label_encoder, class_imbal, lang_imbal)
+        sampler = WeightedRandomSampler(weights, num_samples=len(weights), replacement=True)
+        train_dataloader = DataLoader(
+            train_dataset.remove_columns(columns_to_remove),
+            batch_size=batch_size, sampler=sampler,
+            collate_fn=data_collator_with_language)
+    else:
+        train_dataloader = DataLoader(
+            train_dataset.remove_columns(columns_to_remove),
+            batch_size=batch_size, shuffle=True,
+            collate_fn=data_collator_with_language)
+
+    test_dataloader = DataLoader(
+        test_dataset.remove_columns(columns_to_remove),
+        batch_size=batch_size, collate_fn=data_collator_with_language)
+    
+    return train_dataloader, test_dataloader
+def get_class_weights(data, device):
+
+    class_weights = compute_class_weight(class_weight='balanced',
+                                        classes=np.unique(data['language']),
+                                        y=pd.Series(data['language']))
+    class_weights = torch.tensor(class_weights, dtype=torch.float).to(device)
+
+
+def get_sample_weights(dataset, label_encoder, class_imbal=False, lang_imbal=False):
+    targets = label_encoder.transform(dataset["target"])
+    languages = dataset["language"]
+
+    n = len(dataset)
+    weights = np.ones(n)
+
+    if class_imbal:
+        class_counts = np.bincount(targets)
+        class_weights = 1.0 / class_counts
+        weights *= class_weights[targets]
+
+    if lang_imbal:
+        lang_unique, lang_counts = np.unique(languages, return_counts=True)
+        lang_to_weight = {lang: 1.0 / count for lang, count in zip(lang_unique, lang_counts)}
+        weights *= np.array([lang_to_weight[lang] for lang in languages])
+
+    return torch.DoubleTensor(weights)
---
+++ training2.py
@@ -0,0 +1,137 @@
+import argparse
+import pandas as pd
+import numpy as np
+from datasets import Dataset
+
+import torch
+from torch.optim import AdamW
+
+from transformers import BertTokenizer, BertForSequenceClassification, get_scheduler   
+from sklearn.preprocessing import LabelEncoder
+from tqdm import tqdm
+
+from training_utils import evaluate, freeze_model
+from data_utils2 import get_data, get_language_weights, get_dataloaders, get_class_weights
+
+
+def training(model,
+             train_loader,
+             val_loader,
+             optimizer,
+             label_encoder,
+             device,
+             loss_fn,
+             num_epochs,
+             eval_type,
+             freeze = False, 
+             debug = False):
+    
+    if debug:
+        print("In DEBUG mode")
+
+    model.to(device)
+
+    # Freeze first layers for quicker training
+    if freeze:
+        freeze_model(model)
+
+    # Set up LR scheduling
+    lr_scheduler = get_scheduler(
+        name="linear", optimizer=optimizer,
+        num_warmup_steps=0,
+        num_training_steps=len(train_loader) * num_epochs
+    )
+
+    # Main Training Loop
+    for epoch in range(num_epochs):
+        model.train()
+        loop = tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs}")
+        for batch in loop:
+            languages = batch.pop("language")
+            batch = {k: v.to(device) for k, v in batch.items()}
+            outputs = model(**batch)
+            logits = outputs.logits
+            labels = batch["labels"]
+            losses = loss_fn(logits, labels)
+
+            loss = losses.mean()
+            
+            loss.backward()
+            optimizer.step()
+            lr_scheduler.step()
+            optimizer.zero_grad()
+            loop.set_postfix(loss=loss.item())
+
+        val_metric = evaluate(model, train_loader, label_encoder, device, eval_type, "f1")
+
+    return val_metric
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Fine-tune BERT model with configurable parameters")
+    parser.add_argument("--eval_type", type=str, default="per-lang", help="Evaluation type")
+    parser.add_argument("--pretrain", type=str, default="bert-base-multilingual-cased", help="Pretrained model name")
+    parser.add_argument("--batch_size", type=int, default=16, help="Batch size")
+    parser.add_argument("--learning_rate", type=float, default=2e-5, help="Learning rate")
+    parser.add_argument("--num_epochs", type=int, default=8, help="Number of training epochs")
+    parser.add_argument("--weight_decay", type=float, default=0.01, help="Weight decay")
+    parser.add_argument("--freeze", type=bool , default = False, help="Freeze BERT layers")
+    parser.add_argument("--debug", action="store_true", help="Debug Training")
+    parser.add_argument("--classImbal", action="store_true", help="Class Imbalance")
+    parser.add_argument("--langImbal", action = "store_true", help="Language Imbalance")
+
+    args = parser.parse_args()        
+
+    print("\n=== Training Configuration ===")
+    print(f"Evaluation type     : {args.eval_type}")
+    print(f"Pretrained model    : {args.pretrain}")
+    print(f"Batch size          : {args.batch_size}")
+    print(f"Learning rate       : {args.learning_rate}")
+    print(f"Number of epochs    : {args.num_epochs}")
+    print(f"Weight decay        : {args.weight_decay}")
+    print(f"Freeze BERT layers  : {args.freeze}")
+    print(f"Debug mode          : {args.debug}")
+    print(f"Class imbalance     : {args.classImbal}")
+    print(f"Language imbalance  : {args.langImbal}")
+    print("===============================\n")
+    
+    # Get device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    train_dataset, test_dataset = get_data(args.debug)
+
+    # Tokenizer and label encoding
+    tokenizer = BertTokenizer.from_pretrained(args.pretrain)
+    label_encoder = LabelEncoder()
+    label_encoder.fit(train_dataset["target"])
+    num_labels = len(label_encoder.classes_)  
+
+    # Get dataloaders
+    train_loader, test_loader = get_dataloaders(
+    tokenizer, label_encoder, args.batch_size,
+    train_dataset, test_dataset,
+    class_imbal=args.classImbal,
+    lang_imbal=args.langImbal
+)
+
+    # Create model
+    model = BertForSequenceClassification.from_pretrained(args.pretrain, num_labels=num_labels)
+
+    #Setup optimizer
+    optimizer = AdamW(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+
+    #Setup loss function
+    loss_fn = torch.nn.CrossEntropyLoss(reduction='none')
+
+    training(model,
+             train_loader,
+             test_loader,
+             optimizer,
+             label_encoder,
+             device,
+             loss_fn,
+             num_epochs=args.num_epochs,
+             eval_type=args.eval_type,
+             freeze=args.freeze,
+             debug = args.debug,
+        )
